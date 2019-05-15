261e914ccc3d45b8f5c43c87f7b79348e3e8f3a1
Marcus Brandenburger <bur@zurich.ibm.com>
2019-05-15 12:05:27 +0200

    Add missing license identifier
    This fixes #72 and adds missing license headers.
    It also adds Fabric's license check to the
    make checks target.
    
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- build.mk
+++ build.mk
@@ -1,3 +1,7 @@
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 include $(TOP)/config.mk
 
 # optionlly allow local overriding defaults
--- tlcc_enclave/generate_protos.sh
+++ tlcc_enclave/generate_protos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # set -eux
 
 FABRIC_PATH=${FABRIC_PATH-../../../hyperledger/fabric}
