766f998badf0340ad705f21d7bc55ed1472312b4
Bruno Vavala <bruno.vavala@intel.com>
2019-06-13 10:31:07 -0700

    add cpplinter to check format of cpp, c, h files
    Signed-off-by: Bruno Vavala <bruno.vavala@intel.com>

--- scripts/cpplinter.sh
+++ scripts/cpplinter.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
+# Copyright Intel Corp. 2019 All Rights Reserved.
 # Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# TODO Implement me
+SCRIPT=`realpath $0`
+SCRIPTDIR=`dirname $SCRIPT`
 
+FILES_TO_PARSE=`find $SCRIPTDIR/.. -iname *.cpp -o -iname *.h -o -iname *.c`
+
+RET=0
+
+for FILE in $FILES_TO_PARSE; do
+
+    #skip file checking for certain folders
+    echo $FILE | grep 'ecc_enclave/_build'  > /dev/null && continue
+    echo $FILE | grep 'ecc_enclave/build'   > /dev/null && continue
+    echo $FILE | grep 'tlcc_enclave/nanopb' > /dev/null && continue
+    echo $FILE | grep 'tlcc_enclave/_build' > /dev/null && continue
+    echo $FILE | grep 'tlcc_enclave/build'  > /dev/null && continue
+    echo $FILE | grep 'common/protobuf'     > /dev/null && continue
+    echo $FILE | grep 'common/base64'       > /dev/null && continue
+    echo $FILE | grep 'common/json'         > /dev/null && continue
+
+    if [[ $1 == 'DO_FORMAT' ]]
+    then
+        clang-format -i $FILE
+    else
+
+        clang-format $FILE -output-replacements-xml | grep "</replacement>" > /dev/null &&\
+            echo "ERROR in format: $FILE" && RET=1
+    fi
+done
+
+if [[ $RET != 0 ]]
+then
+    echo "Format check failed. Run '$0 DO_FORMAT' to fix the format."
+fi
+
+exit $RET
