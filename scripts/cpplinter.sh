af7af7c5c61bdccb09e9fb1a482d6eb171d883f0
Bruno Vavala <bruno.vavala@intel.com>
2019-06-20 14:59:45 -0700

    add echo's to lint
    Signed-off-by: Bruno Vavala <bruno.vavala@intel.com>

--- scripts/cpplinter.sh
+++ scripts/cpplinter.sh
@@ -12,18 +12,27 @@ FILES_TO_PARSE=`find $SCRIPTDIR/.. -iname *.cpp -o -iname *.h -o -iname *.c`
 
 RET=0
 
+EXCLUDED_PATTERNS="\
+    ecc_enclave/_build \
+    tlcc_enclave/nanopb \
+    tlcc_enclave/_build \
+    common/protobuf \
+    common/base64 \
+    common/json \
+    enclave_u.c \
+    enclave_u.h \
+    enclave_t.c \
+    enclave_t.h \
+    .pb. \
+    "
 for FILE in $FILES_TO_PARSE; do
 
     #skip file checking for certain folders
-    echo $FILE | grep 'ecc_enclave/_build'  > /dev/null && continue
-    echo $FILE | grep 'ecc_enclave/build'   > /dev/null && continue
-    echo $FILE | grep 'tlcc_enclave/nanopb' > /dev/null && continue
-    echo $FILE | grep 'tlcc_enclave/_build' > /dev/null && continue
-    echo $FILE | grep 'tlcc_enclave/build'  > /dev/null && continue
-    echo $FILE | grep 'common/protobuf'     > /dev/null && continue
-    echo $FILE | grep 'common/base64'       > /dev/null && continue
-    echo $FILE | grep 'common/json'         > /dev/null && continue
+    for EP in $EXCLUDED_PATTERNS; do
+        echo $FILE | grep -F $EP  > /dev/null && continue 2
+    done
 
+    #do format (if specified) or simply check format
     if [[ $1 == 'DO_FORMAT' ]]
     then
         clang-format -i $FILE
@@ -34,6 +43,7 @@ for FILE in $FILES_TO_PARSE; do
     fi
 done
 
+#if check fails, provide instructions for fixing the format
 if [[ $RET != 0 ]]
 then
     echo "Format check failed. Run '$0 DO_FORMAT' to fix the format."
