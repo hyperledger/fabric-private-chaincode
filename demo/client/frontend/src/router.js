2674a7c5268d19c92b3a0afd8e8111cdf1d03a0a
Marcus Brandenburger <bur@zurich.ibm.com>
2020-01-20 11:19:30 +0100

    Remove broken components
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- demo/client/frontend/src/router.js
+++ demo/client/frontend/src/router.js
@@ -9,16 +9,12 @@ import VueRouter from "vue-router";
 import store from "./store";
 import Login from "./views/Login";
 import AuctionInfo from "./views/AuctionInfo";
-import Results from "./views/MyResults";
-import BidSummary from "./views/BidSummary";
 import PlaceBid from "./views/PlaceBid";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/auction_info", component: AuctionInfo },
-  { path: "/my_results", component: Results },
-  { path: "/bid_summary", component: BidSummary },
   { path: "/place_bid", component: PlaceBid },
   { path: "/login", component: Login },
   { path: "/", component: AuctionInfo },
