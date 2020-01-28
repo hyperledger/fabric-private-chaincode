a71ed2309fbb9ce767a90e99165fc5b1590d1cb5
Swetha Repakula <srepaku@us.ibm.com>
2020-01-27 16:46:23 -0800

    Bring up FPC Auction CC in demo
     - fpc auction cc is brought up in end to end scripts as auctioncc
     - fabric-gateway is configured to talk with auction cc
     - add help text for demo start and teardown scripts
     - add flags for startFPCAuctionNetwork to allow flexibility in building
     chaincode and client images
    
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

--- demo/scripts/teardown.sh
+++ demo/scripts/teardown.sh
@@ -7,6 +7,38 @@
 
 set -e
 
+help(){
+   echo "$(basename $0) [options]
+   This script, by default, will teardown all of the auction demo components, but will not prune
+   volumes or delete the chaincode images that have been created for the mockcc and the auctioncc
+   options:
+       --clean-slate:
+           As part of the teardown, prune all unused docker volumes and delete mockcc and auctioncc
+           images.
+       --help,-h:
+           Print this help screen.
+    "
+}
+
+CLEAN_SLATE=false
+for var in "$@"; do
+    case "$var" in
+        "--clean-slate")
+            CLEAN_SLATE=true
+            ;;
+        "-h"|"--help")
+            help
+            exit
+            ;;
+        *)
+            echo "Invalid option: ${var}"
+            help
+            exit
+            ;;
+    esac
+    shift
+done
+
 DEMO_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEMO_DOCKER_COMPOSE=${DEMO_SCRIPT_DIR}/../docker-compose.yml
 
@@ -14,15 +46,22 @@ DEMO_DOCKER_COMPOSE=${DEMO_SCRIPT_DIR}/../docker-compose.yml
 SCRIPT_DIR=${DEMO_SCRIPT_DIR}/../../utils/docker-compose/scripts
 . ${SCRIPT_DIR}/lib/common.sh
 
+
 COMPOSE_IGNORE_ORPHANS=true docker-compose -f ${DEMO_DOCKER_COMPOSE} kill && COMPOSE_IGNORE_ORPHANS=true docker-compose -f ${DEMO_DOCKER_COMPOSE} down
 
-
-if [ "$1" == '--clean-slate' ]; then
+if $CLEAN_SLATE; then
     "${SCRIPT_DIR}/teardown.sh" --clean-slate
-    images=$(docker images dev_*mockcc-* -q)
-    if [ ! -z "${images}" ]; then
-        docker rmi "${images}"
+
+    image=$(go run ${DEMO_SCRIPT_DIR}/../../utils/fabric/get-fabric-container-name.go --cc-name mockcc --peer-id peer0.org1.example.com --net-id dev_test --cc-version 1.0)
+    if [ ! -z "${image}" ]; then
+        docker rmi "${image}"
     fi
-else
-    "${SCRIPT_DIR}/teardown.sh"
+
+    image=$(go run ${DEMO_SCRIPT_DIR}/../../utils/fabric/get-fabric-container-name.go --cc-name ecc_auctioncc --peer-id peer0.org1.example.com --net-id dev_test --cc-version 1.0)
+    if [ ! -z "${image}" ]; then
+        docker rmi "${image}"
+    fi
+    exit
 fi
+
+"${SCRIPT_DIR}/teardown.sh"
