19386ea8227cf70214c6122dc6f3238011a1eac7
Michael Steiner <michael.steiner@intel.com>
2020-02-11 21:07:04 -0800

    * allow re-mapping of MSPId, DN and org for mock server for both requesters   as well as bidders in createAuction requests and getDefaultAuction.
    Signed-off-by: Michael Steiner <michael.steiner@intel.com>

--- demo/client/backend/mock/api/mock_data.go
+++ demo/client/backend/mock/api/mock_data.go
@@ -8,14 +8,28 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
 
+type MappedName struct {
+	User  string `json:"user"`
+	MspId string `json:"mspId"`
+	Org   string `json:"org"`
+}
+
 var MockData map[string]interface{}
+var MockNameMap map[string]MappedName
 
 func init() {
-	MockData, _ = loadMockData()
+	var err error
+	if MockData, err = loadMockData(); err != nil {
+		panic(fmt.Sprintf("Cannot read mock data: error=%v", err))
+	}
+	if MockNameMap, err = loadMockNameMap(); err != nil {
+		panic(fmt.Sprintf("Cannot read mock name mapping: error=%v", err))
+	}
 }
 
 func loadMockData() (map[string]interface{}, error) {
@@ -31,6 +45,27 @@ func loadMockData() (map[string]interface{}, error) {
 	}
 
 	var result map[string]interface{}
+	err = json.Unmarshal(byteValue, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func loadMockNameMap() (map[string]MappedName, error) {
+	jsonFile, err := os.Open("api/name-map.json")
+	defer jsonFile.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]MappedName
 	err = json.Unmarshal(byteValue, &result)
 	if err != nil {
 		return nil, err
