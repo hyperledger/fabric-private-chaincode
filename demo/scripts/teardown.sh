064a8da047d2dcb1415f57734b1a0f645c4f8018
Swetha Repakula <srepaku@us.ibm.com>
2020-02-13 15:36:27 -0800

    Delete chaincode images only if they exist
     - fix fpc chaincode Makefile docker inspect to redirect stderr to
     stdout
    
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

--- demo/scripts/teardown.sh
+++ demo/scripts/teardown.sh
@@ -57,15 +57,11 @@ if $CLEAN_SLATE; then
     "${SCRIPT_DIR}/teardown.sh" --clean-slate
 
     image=$(go run ${DEMO_SCRIPT_DIR}/../../utils/fabric/get-fabric-container-name.go --cc-name mockcc --peer-id peer0.org1.example.com --net-id dev_test --cc-version 1.0)
-    if [ ! -z "${image}" ]; then
-        docker rmi "${image}"
-    fi
+    docker inspect $image > /dev/null 2>&1 && docker rmi "${image}" || echo "No mockcc images available to remove"
 
     image=$(go run ${DEMO_SCRIPT_DIR}/../../utils/fabric/get-fabric-container-name.go --cc-name ecc_auctioncc --peer-id peer0.org1.example.com --net-id dev_test --cc-version 1.0)
-    if [ ! -z "${image}" ]; then
-        docker rmi "${image}"
-    fi
-    exit
+    docker inspect $image > /dev/null 2>&1 && docker rmi "${image}" || echo "No auctioncc images available to remove"
 fi
+
 
 "${SCRIPT_DIR}/teardown.sh"
