8d3741a4a5369b6e1764e14edeef7ca6ba44a645
bur <bur@zurich.ibm.com>
2020-04-28 21:10:30 +0200

    Re-enable tlcc test
    Signed-off-by: bur <bur@zurich.ibm.com>

--- tlcc/tlcc_test.go
+++ tlcc/tlcc_test.go
@@ -9,49 +9,72 @@ package tlcc
 import (
 	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strconv"
 	"testing"
 
 	"github.com/hyperledger-labs/fabric-private-chaincode/tlcc/enclave"
-	. "github.com/hyperledger-labs/fabric-private-chaincode/utils"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-chaincode-go/shimtest"
+	"github.com/hyperledger/fabric/core/ledger/ledgermgmt"
+	"github.com/hyperledger/fabric/core/ledger/ledgermgmt/ledgermgmttest"
 	"github.com/hyperledger/fabric/core/peer"
-	"github.com/spf13/viper"
 )
 
-func setupTestLedger(chainid string) {
-	viper.Set("peer.fileSystemPath", "/tmp/hyperledger/test/")
-	peer.MockInitialize()
-	peer.MockCreateChain(chainid)
+const ChannelId = "mychannel"
+
+func setupTestLedger(cid string) (*shimtest.MockStub, *TrustedLedgerCC, func()) {
+
+	peerInstance := &peer.Peer{}
+
+	tempdir, err := ioutil.TempDir("", "tlcc-test")
+	if err != nil {
+		panic(fmt.Sprintf("failed to create temporary directory: %s", err))
+	}
+
+	initializer := ledgermgmttest.NewInitializer(filepath.Join(tempdir, "ledgerData"))
+	peerInstance.LedgerMgr = ledgermgmt.NewLedgerMgr(initializer)
+
+	cleanup := func() {
+		peerInstance.LedgerMgr.Close()
+		os.RemoveAll(tempdir)
+	}
+
+	err = peer.CreateMockChannel(peerInstance, cid, nil)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create channel: %s", err))
+	}
+
+	tlcc := &TrustedLedgerCC{
+		enclave: &enclave.MockStub{},
+		peer:    peerInstance,
+	}
+
+	stub := shimtest.NewMockStub("tlcc", tlcc)
+	stub.ChannelID = cid
+	return stub, tlcc, cleanup
 }
 
 func TestTrustedLedgerCC_Init(t *testing.T) {
-	tlcc := createTlcc()
-	stub := shimtest.NewMockStub("tlcc", tlcc)
-	stub.ChannelID = "mychannel"
-	setupTestLedger("mychannel")
+	stub, _, cleanup := setupTestLedger(ChannelId)
+	defer cleanup()
 	CheckInit(t, stub, [][]byte{})
 }
 
 func TestTrustedLedgerCC_JoinChannel(t *testing.T) {
-	tlcc := createTlcc()
-	stub := shimtest.NewMockStub("tlcc", tlcc)
-	stub.ChannelID = "mychannel"
-
-	setupTestLedger("mychannel")
+	stub, _, cleanup := setupTestLedger(ChannelId)
+	defer cleanup()
 	CheckInit(t, stub, [][]byte{})
-	CheckInvoke(t, stub, [][]byte{[]byte("JOIN_CHANNEL"), []byte("mychannel")})
+	CheckInvoke(t, stub, [][]byte{[]byte("JOIN_CHANNEL"), []byte(ChannelId)})
 }
 
 func TestTrustedLedgerCC_GetReport(t *testing.T) {
-	tlcc := createTlcc()
-	stub := shimtest.NewMockStub("tlcc", tlcc)
-	stub.ChannelID = "mychannel"
-
-	setupTestLedger("mychannel")
+	stub, tlcc, cleanup := setupTestLedger(ChannelId)
+	defer cleanup()
 	CheckInit(t, stub, [][]byte{})
-	CheckInvoke(t, stub, [][]byte{[]byte("JOIN_CHANNEL"), []byte("mychannel")})
+	CheckInvoke(t, stub, [][]byte{[]byte("JOIN_CHANNEL"), []byte(ChannelId)})
 
 	// note this is a debugging call :D
 	targetInfo, _ := tlcc.GetTargetInfo()
@@ -65,13 +88,10 @@ func TestTrustedLedgerCC_GetReport(t *testing.T) {
 }
 
 func TestTrustedLedgerCC_GetStateCMAC(t *testing.T) {
-	tlcc := createTlcc()
-	stub := shimtest.NewMockStub("tlcc", tlcc)
-	stub.ChannelID = "mychannel"
-
-	setupTestLedger("mychannel")
+	stub, _, cleanup := setupTestLedger(ChannelId)
+	defer cleanup()
 	CheckInit(t, stub, [][]byte{})
-	CheckInvoke(t, stub, [][]byte{[]byte("JOIN_CHANNEL"), []byte("mychannel")})
+	CheckInvoke(t, stub, [][]byte{[]byte("JOIN_CHANNEL"), []byte(ChannelId)})
 
 	key := []byte("some.channel.someKey")
 	nonce := []byte(base64.StdEncoding.EncodeToString([]byte("moin")))
@@ -87,12 +107,18 @@ func TestTrustedLedgerCC_GetStateCMAC(t *testing.T) {
 	fmt.Println("CMAC: " + string(res.Payload))
 }
 
-func TestLoadPlugin(t *testing.T) {
-	CheckLoadPlugin(t, "tlcc.so")
+func CheckInit(t *testing.T, stub *shimtest.MockStub, args [][]byte) {
+	res := stub.MockInit("1", args)
+	if res.Status != shim.OK {
+		fmt.Println("Init failed:", string(res.Message))
+		t.FailNow()
+	}
 }
 
-func createTlcc() *TrustedLedgerCC {
-	return &TrustedLedgerCC{
-		enclave: &enclave.MockStub{},
+func CheckInvoke(t *testing.T, stub *shimtest.MockStub, args [][]byte) {
+	res := stub.MockInvoke("1", args)
+	if res.Status != shim.OK {
+		fmt.Println("Invoke", args, "failed:", string(res.Message))
+		t.FailNow()
 	}
 }
