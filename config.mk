234553f5fe38245c6aef6db7e020549b451f380a
michael steiner <michael.steiner@intel.com>
2020-05-01 20:58:39 -0700

    Make docker-based tests run also inside dev(elopment) container * make sure host network is also available inside * for volume mounts, make sure the source path maps to a path   understood by docker daemon
    Signed-off-by: michael steiner <michael.steiner@intel.com>

--- config.mk
+++ config.mk
@@ -17,6 +17,12 @@ export SGX_BUILD ?= PRERELEASE
 
 export FABRIC_PATH ?= ${GOPATH}/src/github.com/hyperledger/fabric
 
+export FPC_PATH=$(abspath $(TOP))
+# to allow volume mounts from within a dev(elopment) container, 
+# below variable is used for volume mounts and can hence be
+# re-defined to point to the FPC path as seen by the docker daemon
+export DOCKERD_FPC_PATH ?= $(FPC_PATH)
+
 JAVA ?= java
 PLANTUML_JAR ?= plantuml.jar
 PLANTUML_CMD ?= $(JAVA) -jar $(PLANTUML_JAR)
