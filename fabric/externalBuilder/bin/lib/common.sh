c66406691d0cb2fbc3bdb6c20fe819e4d4d3ca8e
Michael Steiner <michael.steiner@intel.com>
2020-07-02 11:44:14 -0700

    Upgrade to Fabric v2.2.0
    - upgrade docker images, scripts, config & docu files to use fabric v2.2
    - also upgrade to go 1.14.4 as needed by fabric v2.2
    
    Signed-off-by: michael steiner <michael.steiner@intel.com>

--- fabric/externalBuilder/bin/lib/common.sh
+++ fabric/externalBuilder/bin/lib/common.sh
@@ -113,7 +113,7 @@ process_runtime_metadata() {
     fi
 
     # For debugging purposes, we also symlink the source metadata & build artifacts
-    # Note: the external builder in fabric 2.1.1 has started treating symlinks differently. So
+    # Note: the external builder in fabric 2.2.0 has started treating symlinks differently. So
     #  far they still work here, so leave it for now as it is still useful for debugging. (But 
     #  as cp doesn't make sense here, we should remove it as soon as symlinks don't work anymore).
     try ln -s "${CC_BUILD_DIR}/" "${RUN_STATE_DIR}/build"
