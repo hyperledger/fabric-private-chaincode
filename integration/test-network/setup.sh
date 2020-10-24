e9169a09671e97a67f1919ce0b3ec0341b37089d
Marcus Brandenburger <bur@zurich.ibm.com>
2020-10-24 22:05:48 +0200

    Add fabric-sample/test-network (#451)
    - add as git submodule
    - provide script to setup test-network using external launcher
    - provide install script for ercc and ecc_mock
    - add initial ecc_mock implementation as a skeleton for the new enclave
      creation/registration flow and lightweight FPC flow
    
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>
    Co-authored-by: Michael Steiner <michael.steiner@intel.com>

---
+++ integration/test-network/setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Copyright IBM Corp. All Rights Reserved.
+# Copyright 2020 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+if [[ -z "${FPC_PATH}" ]]; then
+  echo "Error: FPC_PATH not set"
+  exit 1
+fi
+
+backup() {
+  FILE=$1
+  BACKUP="${FILE}.backup"
+
+  if [[ -e "${BACKUP}" ]]; then
+    cp "${BACKUP}" "${FILE}"
+  else
+    cp "${FILE}" "${BACKUP}"
+  fi
+}
+
+FABRIC_SAMPLES=${FPC_PATH}/integration/test-network/fabric-samples/
+
+#cd ${FABRIC_SAMPLES} && curl -sSL https://bit.ly/2ysbOFE | bash -s -- -s
+
+CORE_PATH=${FABRIC_SAMPLES}/config/core.yaml
+DOCKER_PATH=${FABRIC_SAMPLES}/test-network/docker/docker-compose-test-net.yaml
+
+# TODO the current `setup.sh` has an issue that it is not idempotent
+# In particular, when starting nodes using the ./network up ... script won't work if
+# docker-compose-test-net.yaml contains redundant volume entries!
+# maybe create a backup copy of `core.yaml` and `docker-compose-test-net.yaml` first and always work from there.
+backup ${CORE_PATH}
+
+yq m -i -a=append ${CORE_PATH} core_ext.yaml
+
+peers=("peer0.org1.example.com" "peer0.org2.example.com")
+
+backup ${DOCKER_PATH}
+
+# Also there is another issue with this approach here. When working completely inside the FPC dev-container,
+# the volume mounts won't work. The reason is that the docker daemon provided by the host cannot parse volume paths.
+# For instance, ${FPC_PATH} inside dev-container is `/project/src/github.com/hyperledger-labs/fabric-private-chaincode/`.
+# The correct path from the docker daemon perspective is something like
+# `/Users/marcusbrandenburger/Developer/gocode/src/github.com/hyperledger-labs/fabric-private-chaincode/` (in my case on the mac).
+
+# Using DOCKERED_FPC_PATH fixes our custom volumes but still there is an issue default mounts in `docker-compose-test-net.yaml`
+# TODO need to be fixed
+
+if [ ! -z ${DOCKERD_FPC_PATH+x} ]; then
+  echo "Oo we are in docker mode! we need to use the host fpc path"
+  FPC_PATH=${DOCKERD_FPC_PATH}
+  FABRIC_SAMPLES=${FPC_PATH}/integration/test-network/fabric-samples/
+  echo "set FPC_PATH = ${FPC_PATH}"
+fi
+
+for p in "${peers[@]}"; do
+  yq w -i ${DOCKER_PATH} "services.\"$p\".volumes[+]" "${FPC_PATH}:/opt/gopath/src/github.com/hyperledger-labs/fabric-private-chaincode"
+  yq w -i ${DOCKER_PATH} "services.\"$p\".volumes[+]" "${FABRIC_SAMPLES}/config/core.yaml:/etc/hyperledger/fabric/core.yaml"
+done
