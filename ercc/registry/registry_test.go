221f3a6cfcc0d0b232245678dab3d93c4190a7bf
michael steiner <michael.steiner@intel.com>
2020-10-27 18:01:58 -0700

    Update sdk test
    * change mock to provide attestation rather than evidence
    * fix path bug
    * some README, script and docker-compose massaging so chaincode
      containers are started with correct config
    
    Signed-off-by: michael steiner <michael.steiner@intel.com>

--- ercc/registry/registry_test.go
+++ ercc/registry/registry_test.go
@@ -55,7 +55,7 @@ type identityEvaluator interface {
 var (
 	mrenclave              = `98aed61c91f258a37c68ed4943297695647ec7bbe6008cc111b0a12650ebeb91`
 	channelId              = "MY_TEST_CHANNEL"
-	chaincodeId            = "SOME_CHAINCODE_ID"
+	chaincodeId            = "SOME_CHAINCODE_PKG_ID"
 	enclaveId              = "some enclave id"
 	someSerializedIdentity = []byte("some identity")
 )
