4fe1e467d9dfc66ac4c28e837a25bb78b43442a9
bur <bur@zurich.ibm.com>
2021-01-21 11:05:07 +0100

    fixup! Fix golinter filter
    Signed-off-by: bur <bur@zurich.ibm.com>

--- scripts/golinter.sh
+++ scripts/golinter.sh
@@ -4,20 +4,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# Note: This file is adapted from hyperledger/fabric scripts/golinter.sh (ec81f3e74)
+
 set -e
 
 # shellcheck source=/dev/null
 source "$(cd "$(dirname "$0")" && pwd)/functions.sh"
 
-fabric_dir="$(cd "$(dirname "$0")/.." && pwd)"
-source_dirs=()
-while IFS=$'\n' read -r source_dir; do
-    source_dirs+=("$source_dir")
-done < <(go list -f '{{.Dir}}' ./... | sed s,"${fabric_dir}".,,g | cut -f 1 -d / | sort -u)
-
 echo "Checking with gofmt"
-OUTPUT="$(gofmt -l -s "${source_dirs[@]}")"
-
+OUTPUT="$(gofmt -l -s ./..)"
 OUTPUT="$(filterExcludedAndGeneratedFiles "$OUTPUT")"
 if [ -n "$OUTPUT" ]; then
     echo "The following files contain gofmt errors"
@@ -27,8 +22,7 @@ if [ -n "$OUTPUT" ]; then
 fi
 
 echo "Checking with goimports"
-OUTPUT="$(goimports -l "${source_dirs[@]}")"
-
+OUTPUT="$(goimports -l ./..)"
 OUTPUT="$(filterExcludedAndGeneratedFiles "$OUTPUT")"
 if [ -n "$OUTPUT" ]; then
     echo "The following files contain goimports errors"
@@ -63,7 +57,7 @@ if [ -n "$OUTPUT" ]; then
 fi
 
 echo "Checking with go vet"
-PRINTFUNCS="Print,Printf,Info,Infof,Warning,Warningf,Error,Errorf,Critical,Criticalf,Sprint,Sprintf,Log,Logf,Panic,Panicf,Fatal,Fatalf,Notice,Noticef,Wrap,Wrapf,WithMessage"
+PRINTFUNCS="Debug,Debugf,Print,Printf,Info,Infof,Warning,Warningf,Error,Errorf,Critical,Criticalf,Sprint,Sprintf,Log,Logf,Panic,Panicf,Fatal,Fatalf,Notice,Noticef,Wrap,Wrapf,WithMessage"
 OUTPUT="$(go vet -all -printfuncs "$PRINTFUNCS" ./...)"
 if [ -n "$OUTPUT" ]; then
     echo "The following files contain go vet errors"
