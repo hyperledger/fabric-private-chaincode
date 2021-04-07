37f7fc18cf2a2ccd3faa04dcc3dc493480de3714
Bruno Vavala <bruno.vavala@intel.com>
2021-04-06 20:51:41 -0700

    (breaking commit) add test
    Signed-off-by: Bruno Vavala <bruno.vavala@intel.com>

---
+++ integration/kv_test.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Copyright 2020 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SCRIPTDIR="$(dirname $(readlink --canonicalize ${BASH_SOURCE}))"
+FPC_PATH="${SCRIPTDIR}/.."
+FABRIC_SCRIPTDIR="${FPC_PATH}/fabric/bin/"
+
+: ${FABRIC_CFG_PATH:="${SCRIPTDIR}/config"}
+
+. ${FABRIC_SCRIPTDIR}/lib/common_utils.sh
+. ${FABRIC_SCRIPTDIR}/lib/common_ledger.sh
+
+CC_ID=kv-test
+CC_PATH=${FPC_PATH}/examples/kv-test/_build/lib/
+CC_LANG=fpc-c
+CC_VER="$(cat ${CC_PATH}/mrenclave)"
+CC_SEQ="1"
+CC_EP="OR('SampleOrg.member')" # note that we use .member as NodeOUs is disabled with the crypto material used in the integration tests.
+
+NUM_FAILURES=0
+NUM_TESTS=0
+
+kv_test() {
+    PKG=/tmp/${CC_ID}.tar.gz
+
+    try ${PEER_CMD} lifecycle chaincode package --lang ${CC_LANG} --label ${CC_ID} --path ${CC_PATH} ${PKG}
+    try ${PEER_CMD} lifecycle chaincode install ${PKG}
+
+    PKG_ID=$(${PEER_CMD} lifecycle chaincode queryinstalled | awk "/Package ID: ${CC_ID}/{print}" | sed -n 's/^Package ID: //; s/, Label:.*$//;p')
+
+    try ${PEER_CMD} lifecycle chaincode approveformyorg -o ${ORDERER_ADDR} -C ${CHAN_ID} --package-id ${PKG_ID} --name ${CC_ID} --version ${CC_VER} --sequence ${CC_SEQ} --signature-policy ${CC_EP}
+
+    try ${PEER_CMD} lifecycle chaincode checkcommitreadiness -C ${CHAN_ID} --name ${CC_ID} --version ${CC_VER} --sequence ${CC_SEQ} --signature-policy ${CC_EP}
+
+    try ${PEER_CMD} lifecycle chaincode commit -o ${ORDERER_ADDR} -C ${CHAN_ID} --name ${CC_ID} --version ${CC_VER} --sequence ${CC_SEQ} --signature-policy ${CC_EP}
+
+    try ${PEER_CMD} lifecycle chaincode initEnclave -o ${ORDERER_ADDR} --peerAddresses "localhost:7051" --name ${CC_ID}
+
+    try ${PEER_CMD} lifecycle chaincode querycommitted -C ${CHAN_ID}
+
+    try_out_r ${PEER_CMD} chaincode invoke -o ${ORDERER_ADDR} -C ${CHAN_ID} -n ${CC_ID} -c '{"Function":"put_state", "Args": ["echo-0", "echo-0"]}' --waitForEvent
+    check_result "OK"
+}
+
+# 1. prepare
+para
+say "Preparing KV Test ..."
+# - clean up relevant docker images
+docker_clean ${ERCC_ID}
+
+trap ledger_shutdown EXIT
+
+para
+say "Run kv test"
+
+say "- setup ledger"
+ledger_init
+
+say "- run kvtest"
+kv_test
+
+say "- shutdown ledger"
+ledger_shutdown
+
+para
+if [[ "$NUM_FAILURES" == 0 ]]; then
+    yell "KV Test test PASSED"
+else
+    yell "KV Test had ${NUM_FAILURES} failures out of ${NUM_TESTS} tests"
+    exit 1
+fi
+exit 0
