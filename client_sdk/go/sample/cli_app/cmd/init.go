mbrandenburger/fabric-secure-chaincode

Add k8s-based demo network
Signed-off-by: bur <bur@zurich.ibm.com>

client_sdk/go/sample/helloworld/cmd/init.go
@@ -1,0 +1,27 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package cmd
+
+import (
+	"github.com/hyperledger-labs/fabric-private-chaincode/client_sdk/go/sample/helloworld/pkg"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init [target_peer]",
+	Short: "initialize enclave",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		admin := pkg.NewAdmin(config)
+		defer admin.Close()
+		return admin.InitEnclave(args[0])
+	},
+}

client_sdk/go/sample/helloworld/cmd/root.go
@@ -1,0 +1,70 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/hyperledger-labs/fabric-private-chaincode/client_sdk/go/sample/helloworld/pkg"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "fpcclient",
+		Short: "A demo app that calls FPC chaincode",
+	}
+	config *pkg.Config
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+
+	getStrEnv := func(key string) string {
+		val := os.Getenv(key)
+		if val == "" {
+			panic(fmt.Sprintf("%s not set", key))
+		}
+		return val
+	}
+
+	getBoolEnv := func(key string) bool {
+		val := getStrEnv(key)
+		ret, err := strconv.ParseBool(val)
+		if err != nil {
+			if val == "" {
+				panic(fmt.Sprintf("invalid bool value for %s", key))
+			}
+		}
+		return ret
+	}
+
+	config = &pkg.Config{
+		CorePeerAddress:         getStrEnv("CORE_PEER_ADDRESS"),
+		CorePeerId:              getStrEnv("CORE_PEER_ID"),
+		CorePeerLocalMSPID:      getStrEnv("CORE_PEER_LOCALMSPID"),
+		CorePeerMSPConfigPath:   getStrEnv("CORE_PEER_MSPCONFIGPATH"),
+		CorePeerTLSCertFile:     getStrEnv("CORE_PEER_TLS_CERT_FILE"),
+		CorePeerTLSEnabled:      getBoolEnv("CORE_PEER_TLS_ENABLED"),
+		CorePeerTLSKeyFile:      getStrEnv("CORE_PEER_TLS_KEY_FILE"),
+		CorePeerTLSRootCertFile: getStrEnv("CORE_PEER_TLS_ROOTCERT_FILE"),
+		OrdererCA:               getStrEnv("ORDERER_CA"),
+		ChaincodeId:             getStrEnv("CC_NAME"),
+		ChannelId:               getStrEnv("CHANNEL_NAME"),
+		GatewayConfigPath:       getStrEnv("GATEWAY_CONFIG"),
+	}
+}

hyperledger/fabric-private-chaincode

Signed-off-by: Marcus brandenburger <bur@zurich.ibm.com>

client_sdk/go/sample/cli_app/cmd/init.go
@@ -1,0 +1,27 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package cmd
+
+import (
+	"github.com/hyperledger-labs/fabric-private-chaincode/client_sdk/go/sample/cli_app/pkg"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init [target_peer]",
+	Short: "initialize enclave",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		admin := pkg.NewAdmin(config)
+		defer admin.Close()
+		return admin.InitEnclave(args[0])
+	},
+}

client_sdk/go/sample/cli_app/cmd/root.go
@@ -1,0 +1,70 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/hyperledger-labs/fabric-private-chaincode/client_sdk/go/sample/cli_app/pkg"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "fpcclient",
+		Short: "A demo app that calls FPC chaincode",
+	}
+	config *pkg.Config
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+
+	getStrEnv := func(key string) string {
+		val := os.Getenv(key)
+		if val == "" {
+			panic(fmt.Sprintf("%s not set", key))
+		}
+		return val
+	}
+
+	getBoolEnv := func(key string) bool {
+		val := getStrEnv(key)
+		ret, err := strconv.ParseBool(val)
+		if err != nil {
+			if val == "" {
+				panic(fmt.Sprintf("invalid bool value for %s", key))
+			}
+		}
+		return ret
+	}
+
+	config = &pkg.Config{
+		CorePeerAddress:         getStrEnv("CORE_PEER_ADDRESS"),
+		CorePeerId:              getStrEnv("CORE_PEER_ID"),
+		CorePeerLocalMSPID:      getStrEnv("CORE_PEER_LOCALMSPID"),
+		CorePeerMSPConfigPath:   getStrEnv("CORE_PEER_MSPCONFIGPATH"),
+		CorePeerTLSCertFile:     getStrEnv("CORE_PEER_TLS_CERT_FILE"),
+		CorePeerTLSEnabled:      getBoolEnv("CORE_PEER_TLS_ENABLED"),
+		CorePeerTLSKeyFile:      getStrEnv("CORE_PEER_TLS_KEY_FILE"),
+		CorePeerTLSRootCertFile: getStrEnv("CORE_PEER_TLS_ROOTCERT_FILE"),
+		OrdererCA:               getStrEnv("ORDERER_CA"),
+		ChaincodeId:             getStrEnv("CC_NAME"),
+		ChannelId:               getStrEnv("CHANNEL_NAME"),
+		GatewayConfigPath:       getStrEnv("GATEWAY_CONFIG"),
+	}
+}