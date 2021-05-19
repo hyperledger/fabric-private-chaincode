e4cb19e00c7c5a45c31d6a000528a065db866c6e
Marcus Brandenburger <bur@zurich.ibm.com>
2021-05-19 18:33:47 +0200

    Add stress test harness including larger tx parms
    This test is expected to fail without hyrbid encryption for tx requests.
    
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

---
+++ integration/stress_test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copyright IBM Corp. All Rights Reserved.
+# Copyright 2020 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SCRIPTDIR="$(dirname $(readlink --canonicalize ${BASH_SOURCE}))"
+FPC_PATH="${SCRIPTDIR}/.."
+FABRIC_SCRIPTDIR="${FPC_PATH}/fabric/bin/"
+
+: ${FABRIC_CFG_PATH:="${FPC_PATH}/integration/config"}
+
+. ${FABRIC_SCRIPTDIR}/lib/common_utils.sh
+. ${FABRIC_SCRIPTDIR}/lib/common_ledger.sh
+TEST_NAME=TestStress
+CHAINCODES=(auction_test echo_test)
+CC_PATH=${FPC_PATH}/samples/chaincode/auction/_build/lib/
+CC_LANG=fpc-c
+CC_VER="$(cat ${CC_PATH}/mrenclave)"
+CC_SEQ="1"
+CC_EP="OR('SampleOrg.member')" # note that we use .member as NodeOUs is disabled with the crypto material used in the integration tests.
+
+run_test() {
+    # call sdk test
+    try cd client_sdk/go/stress_test && go test -v
+
+    # cleanup
+    rm -rf keystore wallet
+}
+
+# 1. prepare
+para
+say "Preparing ${TEST_NAME} Test ..."
+# - clean up relevant docker images
+docker_clean ${ERCC_ID}
+
+trap ledger_shutdown EXIT
+
+para
+say "Run ${TEST_NAME} test"
+
+say "- setup ledger"
+ledger_init
+
+say "- run test"
+run_test
+
+say "- shutdown ledger"
+ledger_shutdown
+
+para
+yell "${TEST_NAME} test PASSED"
+exit 0
