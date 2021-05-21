2d008b59dcac004234ae45a4f7ae95db073a9eb0
Bruno Vavala <bruno.vavala@intel.com>
2021-05-21 15:06:35 +0000

    data provider
    Signed-off-by: Bruno Vavala <bruno.vavala@intel.com>

--- samples/demos/irb/data-provider/register.go
+++ samples/demos/irb/data-provider/register.go
@@ -4,7 +4,7 @@
    SPDX-License-Identifier: Apache-2.0
 */
 
-package main
+package data_provider
 
 import (
 	"encoding/base64"
---
+++ samples/demos/irb/data-provider/test/main.go
@@ -0,0 +1,62 @@
+/*
+   Copyright 2021 Intel Corporation
+
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"fmt"
+
+	dp "github.com/hyperledger/fabric-private-chaincode/samples/demos/irb/data-provider"
+)
+
+func main() {
+	users := []string{"user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9"}
+
+	fmt.Printf("Creating users and data...")
+	for i := 0; i < len(users); i++ {
+		_, _, _, err := dp.LoadOrCreateUser(users[i])
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = LoadOrCreateData(users[i])
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Printf("done.\n")
+
+	fmt.Printf("Uploading data...")
+	decryptionKeys := [][]byte{}
+	for i := 0; i < len(users); i++ {
+		data, err := LoadOrCreateData(users[i])
+		if err != nil {
+			panic(err)
+		}
+
+		dk, err := dp.Upload(data)
+		if err != nil {
+			panic(err)
+		}
+
+		decryptionKeys = append(decryptionKeys, dk)
+	}
+	fmt.Printf("done.\n")
+
+	fmt.Printf("Registering data...")
+	for i := 0; i < len(users); i++ {
+		uuid, _, vk, err := dp.LoadOrCreateUser(users[i])
+		if err != nil {
+			panic(err)
+		}
+
+		err = dp.RegisterData(uuid, vk, decryptionKeys[i])
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Printf("done.\n")
+}
