6c662baa663b250ae24e80164d7b12f35df2e595
Marcus Brandenburger <bur@zurich.ibm.com>
2021-09-09 11:35:15 +0200

    Post-Release: Set version to main
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- config.mk
+++ config.mk
@@ -68,7 +68,7 @@ PROJECT_NAME=fabric-private-chaincode
 
 export FABRIC_VERSION ?= 2.3.0
 
-export FPC_VERSION := v1.0-rc2
+export FPC_VERSION := main
 
 export FABRIC_PATH ?= ${GOPATH}/src/github.com/hyperledger/fabric
 
