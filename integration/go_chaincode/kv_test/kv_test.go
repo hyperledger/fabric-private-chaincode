aa217a2de82033d565b247e2ca5a4380c0f53faf
Marcus Brandenburger <bur@zurich.ibm.com>
2022-02-03 16:55:40 +0100

    Fix kvs-go test
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- integration/go_chaincode/kv_test/kv_test.go
+++ integration/go_chaincode/kv_test/kv_test.go
@@ -7,9 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package kv
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/hyperledger-labs/fabric-smart-client/integration"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/common"
@@ -22,31 +20,26 @@ func TestFlow(t *testing.T) {
 	ii, err := integration.Generate(23000, false, Topology()...)
 	assert.NoError(t, err)
 	ii.Start()
-
-	// give me some time
-	fmt.Println("time to sleep!!")
-	time.Sleep(15 * time.Second)
-
 	defer ii.Stop()
 
 	_, err = ii.Client("client").CallView("invoke", common.JSONMarshall(&Client{
-		cid:      ChaincodeName,
-		function: "put_state",
-		args:     []string{"echo-0", "echo-0"},
+		CID:      ChaincodeName,
+		Function: "put_state",
+		Args:     []string{"echo-0", "echo-0"},
 	}))
 	assert.NoError(t, err)
 
 	_, err = ii.Client("client").CallView("invoke", common.JSONMarshall(&Client{
-		cid:      ChaincodeName,
-		function: "get_state",
-		args:     []string{"echo-0"},
+		CID:      ChaincodeName,
+		Function: "get_state",
+		Args:     []string{"echo-0"},
 	}))
 	assert.NoError(t, err)
 
 	_, err = ii.Client("client").CallView("invoke", common.JSONMarshall(&Client{
-		cid:      ChaincodeName,
-		function: "del_state",
-		args:     []string{"echo-0"},
+		CID:      ChaincodeName,
+		Function: "del_state",
+		Args:     []string{"echo-0"},
 	}))
 	assert.NoError(t, err)
 }
