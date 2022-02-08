a81324cf2bc334534f0dc9bbbf6d2bae5dea82bd
Marcus Brandenburger <bur@zurich.ibm.com>
2022-02-08 14:55:41 +0100

    Replace IteratorBuffer with Iterator decorator
    - Create fpc Iterator that is responsible for decryption and rwset
      handling
    - Remove buffer implementation of iterator
    - Refactor rwset impl
    
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- ecc_go/chaincode/enclave_go/shim.go
+++ ecc_go/chaincode/enclave_go/shim.go
@@ -8,22 +8,16 @@ SPDX-License-Identifier: Apache-2.0
 package enclave_go
 
 import (
-	"crypto/sha256"
-	"fmt"
-
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
-	"github.com/hyperledger/fabric-private-chaincode/internal/protos"
 	"github.com/hyperledger/fabric-private-chaincode/internal/utils"
-	"github.com/hyperledger/fabric-protos-go/ledger/queryresult"
-	"github.com/hyperledger/fabric-protos-go/ledger/rwset/kvrwset"
 	pb "github.com/hyperledger/fabric-protos-go/peer"
 )
 
 type FpcStubInterface struct {
 	stub  shim.ChaincodeStubInterface
 	input *pb.ChaincodeInput
-	rwset *readWriteSet
+	rwset ReadWriteSet
 	sep   StateEncryptionFunctions
 }
 
@@ -33,50 +27,6 @@ func NewFpcStubInterface(stub shim.ChaincodeStubInterface, input *pb.ChaincodeIn
 		input: input,
 		sep:   sep,
 		rwset: rwset,
-	}
-}
-
-type readWriteSet struct {
-	reads  map[string]read
-	writes map[string]write
-}
-
-type read struct {
-	kvread *kvrwset.KVRead
-	hash   []byte
-}
-
-type write struct {
-	kvwrite *kvrwset.KVWrite
-}
-
-func (rwset *readWriteSet) toFPCKVSet() *protos.FPCKVSet {
-	fpcKVSet := &protos.FPCKVSet{
-		RwSet: &kvrwset.KVRWSet{
-			Reads:  []*kvrwset.KVRead{},
-			Writes: []*kvrwset.KVWrite{},
-		},
-		ReadValueHashes: [][]byte{},
-	}
-
-	// fill with reads
-	for _, read := range rwset.reads {
-		fpcKVSet.RwSet.Reads = append(fpcKVSet.RwSet.Reads, read.kvread)
-		fpcKVSet.ReadValueHashes = append(fpcKVSet.ReadValueHashes, read.hash)
-	}
-
-	// fill with writes
-	for _, write := range rwset.writes {
-		fpcKVSet.RwSet.Writes = append(fpcKVSet.RwSet.Writes, write.kvwrite)
-	}
-
-	return fpcKVSet
-}
-
-func newReadWriteSet() *readWriteSet {
-	return &readWriteSet{
-		reads:  make(map[string]read),
-		writes: make(map[string]write),
 	}
 }
 
@@ -115,12 +65,10 @@ func (f *FpcStubInterface) GetArgsSlice() ([]byte, error) {
 }
 
 func (f *FpcStubInterface) GetTxID() string {
-	// TODO double check
 	return f.stub.GetTxID()
 }
 
 func (f *FpcStubInterface) GetChannelID() string {
-	// TODO double check
 	return f.stub.GetChannelID()
 }
 
@@ -142,13 +90,7 @@ func (f *FpcStubInterface) GetPublicState(key string) ([]byte, error) {
 		return nil, err
 	}
 
-	f.rwset.reads[key] = read{
-		kvread: &kvrwset.KVRead{
-			Key:     key,
-			Version: nil,
-		},
-		hash: hash(value),
-	}
+	f.rwset.AddRead(key, hash(value))
 
 	return value, nil
 }
@@ -162,13 +104,7 @@ func (f *FpcStubInterface) PutState(key string, value []byte) error {
 }
 
 func (f *FpcStubInterface) PutPublicState(key string, value []byte) error {
-	f.rwset.writes[key] = write{
-		kvwrite: &kvrwset.KVWrite{
-			Key:      key,
-			IsDelete: false,
-			Value:    value,
-		},
-	}
+	f.rwset.AddWrite(key, value)
 
 	// note that since we are not using the fabric proposal response  we can skip the putState call
 	//return f.stub.PutState(key, value)
@@ -176,12 +112,7 @@ func (f *FpcStubInterface) PutPublicState(key string, value []byte) error {
 }
 
 func (f *FpcStubInterface) DelState(key string) error {
-	f.rwset.writes[key] = write{
-		kvwrite: &kvrwset.KVWrite{
-			Key:      key,
-			IsDelete: true,
-		},
-	}
+	f.rwset.AddDelete(key)
 
 	// note that since we are not using the fabric proposal response  we can skip the delState call
 	//return f.stub.DelState(key)
@@ -204,96 +135,23 @@ func (f *FpcStubInterface) GetStateByRangeWithPagination(startKey string, endKey
 	panic("not implemented") // TODO: Implement
 }
 
-// Quick and Dirty
-// Really
-type IteratorBuffer struct {
-	buffer []*queryresult.KV
-	index  int
-}
-
-func (i *IteratorBuffer) Add(in *queryresult.KV) {
-	i.buffer = append(i.buffer, in)
-}
-
-func (i *IteratorBuffer) HasNext() bool {
-	return i.index < len(i.buffer)
-}
-
-func (i *IteratorBuffer) Close() error {
-	return nil
-}
-
-func (i *IteratorBuffer) Next() (*queryresult.KV, error) {
-	out := i.buffer[i.index]
-	i.index = i.index + 1
-	return out, nil
-}
-
 func (f *FpcStubInterface) GetStateByPartialCompositeKey(objectType string, keys []string) (shim.StateQueryIteratorInterface, error) {
-	fmt.Println("Private start")
-	fmt.Println("objectType", objectType)
-	fmt.Println("keys", keys)
-	iterator, err := f.GetPublicStateByPartialCompositeKey(objectType, keys)
-	if err != nil {
-		return nil, err
-	}
-	buffer := &IteratorBuffer{}
-	for iterator.HasNext() {
-		i, err := iterator.Next()
-		if err != nil {
-			return nil, err
-		}
-		decValue, err := f.sep.DecryptState(i.Value)
-		if err != nil {
-			return nil, err
-		}
-		b := &queryresult.KV{
-			Namespace: i.Namespace,
-			Key:       i.Key,
-			Value:     decValue,
-		}
-		buffer.Add(b)
-		fmt.Println(i.Key, "Key")
-		fmt.Println(decValue, "DecValue")
-	}
-	fmt.Println("Private end")
-	return buffer, nil
-}
-
-func (f *FpcStubInterface) GetPublicStateByPartialCompositeKey(objectType string, keys []string) (shim.StateQueryIteratorInterface, error) {
-	fmt.Println("Public start")
-	fmt.Println("objectType", objectType)
-	fmt.Println("keys", keys)
 	iterator, err := f.stub.GetStateByPartialCompositeKey(objectType, keys)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(&iterator)
-	buffer := &IteratorBuffer{}
-	for iterator.HasNext() {
-		i, err := iterator.Next()
-		if err != nil {
-			return nil, err
-		}
-		v_hash := hash(i.Value)
-		fmt.Println(i.Key, "Key")
-		fmt.Println(utils.TransformToFPCKey(i.Key), "FPC")
-		fmt.Println(i.Value, "Value")
-		fmt.Println(v_hash)
-		//f.fpcKvSet.RwSet.Reads = append(f.fpcKvSet.RwSet.Reads, &kvrwset.KVRead{
-		//	Key:     utils.TransformToFPCKey(i.Key),
-		//	Version: nil,
-		//})
-		//f.fpcKvSet.ReadValueHashes = append(f.fpcKvSet.ReadValueHashes, v_hash[:])
-		b := &queryresult.KV{
-			Namespace: i.Namespace,
-			Key:       utils.TransformToFPCKey(i.Key),
-			Value:     i.Value,
-		}
-		buffer.Add(b)
+
+	return newFpcIterator(iterator, f.rwset.AddRead, f.sep.DecryptState), nil
+}
+
+func (f *FpcStubInterface) GetPublicStateByPartialCompositeKey(objectType string, keys []string) (shim.StateQueryIteratorInterface, error) {
+	iterator, err := f.stub.GetStateByPartialCompositeKey(objectType, keys)
+	if err != nil {
+		return nil, err
 	}
-	fmt.Println("Public end")
-	return buffer, nil
+
+	// note that we do not pass the state decryption function here
+	return newFpcIterator(iterator, f.rwset.AddRead, nil), nil
 }
 
 func (f *FpcStubInterface) GetStateByPartialCompositeKeyWithPagination(objectType string, keys []string, pageSize int32, bookmark string) (shim.StateQueryIteratorInterface, *pb.QueryResponseMetadata, error) {
@@ -309,7 +167,6 @@ func (f *FpcStubInterface) CreateCompositeKey(objectType string, attributes []st
 }
 
 func (f *FpcStubInterface) SplitCompositeKey(compositeKey string) (string, []string, error) {
-	// TODO
 	panic("not implemented") // TODO: Implement
 }
 
@@ -362,7 +219,6 @@ func (f *FpcStubInterface) GetPrivateDataQueryResult(collection string, query st
 }
 
 func (f *FpcStubInterface) GetCreator() ([]byte, error) {
-	// TODO double check
 	return f.stub.GetCreator()
 }
 
@@ -379,7 +235,6 @@ func (f *FpcStubInterface) GetDecorations() map[string][]byte {
 }
 
 func (f *FpcStubInterface) GetSignedProposal() (*pb.SignedProposal, error) {
-	// TODO double check
 	return f.stub.GetSignedProposal()
 }
 
@@ -389,10 +244,4 @@ func (f *FpcStubInterface) GetTxTimestamp() (*timestamp.Timestamp, error) {
 
 func (f *FpcStubInterface) SetEvent(name string, payload []byte) error {
 	panic("not implemented") // TODO: Implement
-}
-
-func hash(value []byte) []byte {
-	h := sha256.New()
-	h.Write(value)
-	return h.Sum(nil)
 }
