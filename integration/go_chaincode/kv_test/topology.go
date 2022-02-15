4a8e8c8651b666ac121a2b42f5a958c3f60d0907
Marcus Brandenburger <bur@zurich.ibm.com>
2022-02-15 10:23:04 +0100

    Add support for HW mode
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- integration/go_chaincode/kv_test/topology.go
+++ integration/go_chaincode/kv_test/topology.go
@@ -7,22 +7,49 @@ SPDX-License-Identifier: Apache-2.0
 package kv
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/api"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fabric"
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fabric/topology"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc"
+	"github.com/hyperledger/fabric-private-chaincode/integration/go_chaincode/utils"
 )
 
 const (
 	ChaincodeName      = "kv-test"
-	ChaincodeImageName = "fpc/fpc-kv-test-go:main"
+	ChaincodeImageName = "fpc/fpc-kv-test-go"
+	ChaincodeImageTag  = "latest"
 )
 
-//const ChaincodeImageName = "fpc/auction"
-
 func Topology() []api.Topology {
+	chaincodeImageName := fmt.Sprintf("%s:%s", ChaincodeImageName, ChaincodeImageTag)
+	var fpcOptions []func(chaincode *topology.ChannelChaincode)
+
+	if strings.ToUpper(os.Getenv("SGX_MODE")) == "HW" {
+		chaincodeImageName = fmt.Sprintf("%s-hw:%s", ChaincodeImageName, ChaincodeImageTag)
+		fpcOptions = append(fpcOptions, topology.WithSGXMode("HW"))
+
+		mrenclave, err := utils.ReadMrenclaveFromFile("mrenclave")
+		if err != nil {
+			panic(errors.Wrapf(err, "cannot get mrenclave"))
+		}
+		fpcOptions = append(fpcOptions, topology.WithMREnclave(mrenclave))
+
+		sgxDevicePath, err := utils.DetectSgxDevicePath()
+		if err != nil {
+			panic(errors.Wrapf(err, "SGX HW mode set but now sgx device found"))
+		}
+		fpcOptions = append(fpcOptions, topology.WithSGXDevicePath(sgxDevicePath))
+	}
+
 	fabricTopology := fabric.NewDefaultTopology()
 	fabricTopology.AddOrganizationsByName("Org1")
-	fabricTopology.AddFPC(ChaincodeName, ChaincodeImageName)
+	fabricTopology.AddFPC(ChaincodeName, chaincodeImageName, fpcOptions...)
 	fabricTopology.SetLogging("fpc=debug:grpc=error:comm.grpc=error:gossip=warning:info", "")
 	fscTopology := fsc.NewTopology()
 
---
+++ integration/go_chaincode/utils/utils.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func DetectSgxDevicePath() (string, error) {
+	possiblePaths := []string{"/dev/isgx", "dev/sgx/enclave"}
+	for _, p := range possiblePaths {
+		if _, err := os.Stat(p); err != nil {
+			continue
+		} else {
+			// first found path returns
+			return p, nil
+		}
+	}
+
+	return "", fmt.Errorf("no sgx device path found")
+}
+
+func ReadMrenclaveFromFile(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("cannot read mrenclave from %s", path)
+	}
+
+	mrenclave := strings.TrimSpace(string(data))
+	if len(mrenclave) == 0 {
+		return "", fmt.Errorf("mrenclave file empty")
+	}
+
+	return mrenclave, nil
+}
