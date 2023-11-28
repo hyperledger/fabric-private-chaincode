014b256746d79779e968978f88912ae299e2e44a
Marcus Brandenburger <bur@zurich.ibm.com>
2023-11-28 13:51:08 +0100

    Add echo-go sample
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

---
+++ samples/chaincode/echo-go/chaincode/echo.go
@@ -0,0 +1,27 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package chaincode
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	pb "github.com/hyperledger/fabric-protos-go/peer"
+)
+
+type Echo struct {
+}
+
+func (t *Echo) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+func (t *Echo) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	functionName, params := stub.GetFunctionAndParameters()
+	fmt.Println("EchoCC: Function:", functionName, "Params:", params)
+	return shim.Success([]byte(functionName))
+}
