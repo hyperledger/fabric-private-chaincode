28b2e1a524647687c81c8506b21b7bb531b49e07
osamamagdy <osamamagdy174@gmail.com>
2024-09-16 02:00:39 +0300

    add uodate external connection script
    Signed-off-by: osamamagdy <osamamagdy174@gmail.com>

--- commands.sh
+++ commands.sh
@@ -1,53 +1,24 @@
 # ############################## Commands in order #######################################
 
 
-# #################Inside the $FPC_PATH
-
-# #  make -C $FPC_PATH/utils/docker pull pull-dev
-# ### which translates to:
-#     docker  pull ghcr.io/hyperledger/fabric-private-chaincode-ccenv:main
-#     docker  image tag ghcr.io/hyperledger/fabric-private-chaincode-ccenv:main hyperledger/fabric-private-chaincode-ccenv:main
-#     docker  pull ghcr.io/hyperledger/fabric-private-chaincode-base-dev:main
-#     docker  image tag ghcr.io/hyperledger/fabric-private-chaincode-base-dev:main hyperledger/fabric-private-chaincode-base-dev:main
-
-# #  make -C $FPC_PATH/utils/docker build build-dev
-# ### which translates to:
-#     docker  build --build-arg FPC_VERSION=main  -t hyperledger/fabric-private-chaincode-base-rt:main base-rt
-#     docker  build --build-arg FPC_VERSION=main -t hyperledger/fabric-private-chaincode-ccenv:main ccenv
-#     docker  build --build-arg FPC_VERSION=main  -t hyperledger/fabric-private-chaincode-base-dev:main base-dev
-
-# #  make -C $FPC_PATH/utils/docker run-dev
-# ### which translates to:
-#     docker  build --build-arg FPC_VERSION=main -t hyperledger/fabric-private-chaincode-dev:main -f ./utils/docker/dev/Dockerfile . 
-
-
-#     docker  run --rm -v "/var/run/docker.sock":"/var/run/docker.sock" -v "/src/github.com/hyperledger/fabric-private-chaincode":/project/src/github.com/hyperledger/fabric-private-chaincode --env DOCKERD_FPC_PATH=/src/github.com/hyperledger/fabric-private-chaincode/ --net=host --env SGX_MODE=SIM -i -e CI=true --name fpc-development-main -t hyperledger/fabric-private-chaincode-dev:main
-
-# #################Inside the fpc container
-#     docker  build --build-arg FPC_VERSION=main  -t hyperledger/fabric-private-chaincode-base-rt:main base-rt
-
-#     docker  build --build-arg FPC_VERSION=main -t hyperledger/fabric-private-chaincode-ccenv:main ccenv
-
-
-#     # There was make build command which was very long to right it down here 
-#     # cd $FPC_PATH
-#     cd samples/chaincode/cc-tools-demo && go get && cd $FPC_PATH
-#     go mod vendor 
-#     # make docker
-#     # make build
-
 # ################################### For CC-tools inside the fpc
 
 # - Copy all chaincode files using the same way as simple-asset-go
+# - replace the "CHAINCODE_ID" env with "CHAINCODE_PKG_ID" in main.go
 # - Run `go get` inside the cc-tools-demo folder after putting it inside the fpc repo
 # ------ There are huge problem with using FPC outside the FPC repository. Even go get doesn't work and you need to specify a certain version and there are conflicting packages-----------------'
-# - You have to add dummy implementation for the PurgePrivate data method in the MockStup of cc-tools but be careful you need to do it in the package installed inside the FPC dev env not your local
-# 	For example do: vim /project/pkg/mod/github.com/hyperledger-labs/cc-tools@v1.0.0/mock/mockstub.go and add this:
-# 	// PurgePrivateData ...
-# 	func (stub *MockStub) PurgePrivateData(collection, key string) error {
-# 		return errors.New("Not Implemented")
-# 	}
-# - replace the "CHAINCODE_ID" env with "CHAINCODE_PKG_ID" in main.go
+# # - You have to add dummy implementation for the PurgePrivate data method in the MockStup of cc-tools but be careful you need to do it in the package installed inside the FPC dev env not your local
+# # 	For example do: vim /project/pkg/mod/github.com/hyperledger-labs/cc-tools@v1.0.0/mock/mockstub.go and add this:
+# # 	// PurgePrivateData ...
+# # 	func (stub *MockStub) PurgePrivateData(collection, key string) error {
+# # 		return errors.New("Not Implemented")
+# # 	}.
+# # A good idea is to use go mod vendor and download all go packages in the vendor directory and edit it one time there. 
+# # nano $FPC_PATH/vendor/github.com/hyperledger-labs/cc-tools/mock/mockstub.go
+# # 	// PurgePrivateData ...
+# # 	func (stub *MockStub) PurgePrivateData(collection, key string) error {
+# # 		return errors.New("Not Implemented")
+# # 	}
 
 cd $FPC_PATH/samples/deployment/test-network
 docker compose down
@@ -58,11 +29,6 @@ docker system prune
 sleep 5
 
 cd $FPC_PATH/samples/chaincode/cc-tools-demo/
-# #   nano $FPC_PATH/vendor/github.com/hyperledger-labs/cc-tools/mock/mockstub.go
-# # 	// PurgePrivateData ...
-	# func (stub *MockStub) PurgePrivateData(collection, key string) error {
-	# 	return errors.New("Not Implemented")
-	# }
 export CC_NAME=fpc-cc-tools-demo
 make
 
@@ -85,9 +51,13 @@ sleep 5
 make ercc-ecc-start
 sleep 5
 
-# prepare connections profile
+# # prepare connections profile
 cd $FPC_PATH/samples/deployment/test-network
 ./update-connection.sh
+
+# # update the connection profile for external clients outside the fpc dev environment
+cd $FPC_PATH/samples/deployment/test-network
+./update-external-connection.sh
 
 # make fpcclient
 cd $FPC_PATH/samples/application/simple-cli-go
@@ -111,21 +81,17 @@ sleep 5
 # init our enclave
 ./fpcclient init $CORE_PEER_ID
 sleep 5
-./fpcclient invoke createNewLibrary "{\"name\":\"samuel\"}"
-sleep 5
-./fpcclient invoke createAsset "{\"asset\":[{\"@assetType\":\"person\",\"id\":\"51027337023\",\"name\":\"samuel\"}]}"
-sleep 5
-./fpcclient invoke createAsset "{\"asset\":[{\"@assetType\":\"book\", \"title\": \"Fairy tail\"  ,\"author\":\"Martin\",\"currentTenant\":{\"@assetType\": \"person\", \"@key\": \"person:f6c10e69-32ae-5dfb-b17e-9eda4a039cee\"}}]}"
-sleep 5
-# ./fpcclient invoke getBooksByAuthor "{\"name\":\"samuel\"}" --> Fails as GetQueryResult is not implemented. I tried to implement it but the fabric implementation needs what's called handler and it's not there
+# invoke the getSchema transaction which is implemented internally by cc-tools
+./fpcclient invoke getSchema
 
-
-# ################################# Now how to run and test cc-tools????????????????????????????????
-
-
-# # -     cd ./ccapi; docker-compose up -d; cd ..
-
-
-
+########################## Some transactions to test ####################################
+# sleep 5
+# ./fpcclient invoke createNewLibrary "{\"name\":\"samuel\"}"
+# sleep 5
+# ./fpcclient invoke createAsset "{\"asset\":[{\"@assetType\":\"person\",\"id\":\"51027337023\",\"name\":\"samuel\"}]}"
+# sleep 5
+# ./fpcclient invoke createAsset "{\"asset\":[{\"@assetType\":\"book\", \"title\": \"Fairy tail\"  ,\"author\":\"Martin\",\"currentTenant\":{\"@assetType\": \"person\", \"@key\": \"person:f6c10e69-32ae-5dfb-b17e-9eda4a039cee\"}}]}"
+# sleep 5
+# ./fpcclient invoke getBooksByAuthor "{\"authorName\":\"samuel\"}" # --> Fails as GetQueryResult is not implemented. I tried to implement it but the fabric implementation needs what's called handler and it's not ther
 
 
---
+++ samples/deployment/test-network/update-external-connection.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# Copyright IBM Corp. All Rights Reserved.
+# Copyright 2020 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+if [[ -z "${FPC_PATH}" ]]; then
+  echo "Error: FPC_PATH not set"
+  exit 1
+fi
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  rm -rf "${tmp_dir}"
+}
+
+backup() {
+  FILE=$1
+  BACKUP="${FILE}.backup"
+
+  if [[ -e "${BACKUP}" ]]; then
+    cp "${BACKUP}" "${FILE}"
+  else
+    cp "${FILE}" "${BACKUP}"
+  fi
+}
+
+orgs=("org1" "org2")
+user="Admin"
+
+shopt -s nullglob
+
+for org in "${orgs[@]}"; do
+
+  ORG_PATH=${FPC_PATH}/samples/deployment/test-network/fabric-samples/test-network/organizations/peerOrganizations/${org}.example.com
+  EXTERNAL_CONNECTIONS_PATH=${ORG_PATH}/external-connection-${org}.yaml
+  CONNECTIONS_PATH=${ORG_PATH}/connection-${org}.yaml
+
+  # Copy the file from the connection profile
+  cp "${CONNECTIONS_PATH}" "${EXTERNAL_CONNECTIONS_PATH}"
+  
+  backup "${EXTERNAL_CONNECTIONS_PATH}"
+
+  # edit localhost urls to use hostnames
+  ## At the root level
+  
+  yq eval ".\"peer0.org1.example.com\".url = \"grpcs://peer0.org1.example.com:7051\"" -i "$EXTERNAL_CONNECTIONS_PATH"
+  yq eval ".\"peer0.org2.example.com\".url = \"grpcs://peer0.org2.example.com:9051\"" -i "$EXTERNAL_CONNECTIONS_PATH"
+
+  ## At the peers level
+  yq eval ".peers.\"peer0.org1.example.com\".url = \"grpcs://peer0.org1.example.com:7051\"" -i "$EXTERNAL_CONNECTIONS_PATH"
+  yq eval ".peers.\"peer0.org2.example.com\".url = \"grpcs://peer0.org2.example.com:9051\"" -i "$EXTERNAL_CONNECTIONS_PATH"
+
+  ## At the CA level
+  yq eval ".certificateAuthorities.\"ca.org1.example.com\".url = \"https://ca.org1.example.com:7054\"" -i "$EXTERNAL_CONNECTIONS_PATH"
+  yq eval ".certificateAuthorities.\"ca.org2.example.com\".url = \"https://ca.org2.example.com:8054\"" -i "$EXTERNAL_CONNECTIONS_PATH"
+
+
+
+
+
+  # remove entity matcher
+  yq eval 'del(.entityMatchers)' -i "$EXTERNAL_CONNECTIONS_PATH"
+
+done
+
+echo "Updated!"
