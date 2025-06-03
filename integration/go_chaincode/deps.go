0d8e58af9480372ce34b4493a247fbfd5bd473f4
Marcus Brandenburger <bur@zurich.ibm.com>
2025-06-03 19:20:47 +0200

    go mod tidy
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

---
+++ integration/go_chaincode/deps.go
@@ -0,0 +1,14 @@
+//go:build deps
+// +build deps
+
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package go_chaincode
+
+import (
+	_ "github.com/hyperledger-labs/fabric-smart-client/node"
+)
